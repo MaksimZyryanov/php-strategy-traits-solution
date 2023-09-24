e49b55456cfb8e931d72e4821c41e117ba0a9db9
Maksim Zyryanov <m.zyryanov@ylab.io>
2023-09-24 17:54:44 +0300

    trait solution

--- Traits.php
+++ Traits.php
@@ -1,5 +1,49 @@
-<?php
+<?php /** @noinspection PhpMultipleClassDeclarationsInspection */
 
 /**
  * Проблема может решиться с помощью трейтов:
  */
+
+interface ITree
+{
+}
+
+/**
+ * Срыв листка
+ */
+interface IGetLeafAction
+{
+    public function getLeaf(): void;
+}
+
+/**
+ * Осенний листопад
+ */
+interface IFallLeavesAction
+{
+    public function fallLeaves(): void;
+}
+
+/**
+ * Снять с дерева фрукт
+ */
+interface IGetFruitAction
+{
+    public function getFruit(): void;
+}
+
+trait DeciduousLeafTrait
+{
+    public function getLeaf(): void
+    {
+        echo 'Листок!';
+    }
+}
+
+/*
+ * Пальма
+ */
+class Palm implements ITree, IGetLeafAction
+{
+    use DeciduousLeafTrait;
+}
